42557800c45d058cdea750b25a519bec17492c65
TON618OFF <rusblackgod@gmail.com>
2024-04-22 13:08:20 +0300

    Добавьте файлы проекта.

---
+++ Models/ImageViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace practice_5.Models
+{
+    internal class ImageViewModel
+    {
+        public List<BitmapImage> Images { get; set; }
+
+        public ImageViewModel(List<string> imagePaths)
+        {
+            Images = new List<BitmapImage>();
+            foreach (string imagePath in imagePaths)
+            {
+                BitmapImage image = new BitmapImage();
+                image.BeginInit();
+                image.UriSource = new System.Uri(imagePath);
+                image.EndInit();
+                Images.Add(image);
+            }
+        }
+
+        public BitmapImage Image1 => Images[0];
+        public BitmapImage Image2 => Images[1];
+        public BitmapImage Image3 => Images[2];
+        public BitmapImage Image4 => Images[3];
+        public BitmapImage Image5 => Images[4];
+        public BitmapImage Image6 => Images[5];
+        public BitmapImage Image7 => Images[6];
+        public BitmapImage Image8 => Images[7];
+        public BitmapImage Image9 => Images[8];
+    }
+}
---
+++ Views/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using practice_5.Models;
+
+namespace practice_5
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Путь к папке с изображениями
+            string directoryPath = "D:\\Vyacheslav\\Study\\Project\\Visual C#\\WPFs\\practice 5\\practice 5\\Images";
+
+            // Получение списка путей к изображениям в папке
+            List<string> imagePaths = new List<string>();
+            foreach (string file in Directory.GetFiles(directoryPath))
+            {
+                imagePaths.Add(file);
+            }
+
+            // Создание объекта ViewModel
+            ImageViewModel viewModel = new ImageViewModel(imagePaths);
+
+            // Привязка ViewModel к окну
+            DataContext = viewModel;
+        }
+    }
+}
